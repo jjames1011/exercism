68fea3a74f01b31cae1a2b8996b0ba53c3f9d6e8
Jonathan James <jjamesnwa1011@gmail.com>
2017-10-27 19:34:38 -0700

    cleaned up white space

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -3,8 +3,6 @@ def is_leap_year(year):
         if year % 100 == 0:
             if year % 400 == 0:
                 return True
-
             return False
         return True
-
     return False
