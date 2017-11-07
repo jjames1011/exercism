dd95f9e2913a8e3e8750222ad7dc4c4e8e31391d
Jonathan James <jjamesnwa1011@gmail.com>
2017-11-06 21:47:34 -0800

    added pangram solution

---
+++ python/pangram/pangram.py
@@ -0,0 +1,16 @@
+def is_pangram(sentence):
+    char_dict = {'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'o':0,
+    'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0}
+    sentence = sentence.replace(" ", "")
+    sentence = sentence.lower()
+    for char in sentence:
+        if char in char_dict:
+            char_dict[char] += 1
+    for key in char_dict:
+        if char_dict[key] == 0:
+            return False
+    return True
+
+
+
+print(is_pangram('hello everyone'))
