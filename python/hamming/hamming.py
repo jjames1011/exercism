405983f681aa0015a79a77324d7a96c7f01e93ef
Jonathan James <jjamesnwa1011@gmail.com>
2017-11-08 19:26:46 -0800

    Added hamming solution

---
+++ python/hamming/hamming.py
@@ -0,0 +1,11 @@
+
+def distance(strand_a, strand_b):
+    hemming = 0
+    i = 0
+    if len(strand_a) != len(strand_b):
+        raise ValueError('The DNA strands must be the same length')
+    for char in strand_a:
+        if strand_b[i] != strand_a[i]:
+            hemming += 1
+        i+=1
+    return hemming
