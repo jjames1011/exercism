05e90823d48c721893aa5c23ddfea566bfbfe14b
Jonathan James <jjamesnwa1011@gmail.com>
2017-11-25 23:28:42 -0800

    working on solution, only failing 5 tests

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -4,8 +4,85 @@ def meetup_day(year, month, day_of_the_week, which):
     for day in days_of_week.keys():
         if day == day_of_the_week:
             day_of_the_week = days_of_week[day]
-
-    if date(year,month, 13).weekday() == day_of_the_week:
-        return date(year,month,13)
-    
-print(meetup_day(2013, 5, 'Monday', 'teenth'))
+    if which == 'teenth':
+        if date(year,month, 13).weekday() == day_of_the_week:
+            return date(year,month,13)
+        if date(year,month, 14).weekday() == day_of_the_week:
+            return date(year,month,14)
+        if date(year,month, 15).weekday() == day_of_the_week:
+            return date(year,month,15)
+        if date(year,month, 16).weekday() == day_of_the_week:
+            return date(year,month,16)
+        if date(year,month, 17).weekday() == day_of_the_week:
+            return date(year,month,17)
+        if date(year,month, 18).weekday() == day_of_the_week:
+            return date(year,month,18)
+        if date(year,month, 19).weekday() == day_of_the_week:
+            return date(year,month,19)
+    if which == '1st':
+        if date(year,month,1).weekday() == day_of_the_week:
+            return date(year,month,1)
+        if date(year,month,2).weekday() == day_of_the_week:
+            return date(year,month,2)
+        if date(year,month,3).weekday() == day_of_the_week:
+            return date(year,month,3)
+        if date(year,month,4).weekday() == day_of_the_week:
+            return date(year,month,4)
+        if date(year,month,5).weekday() == day_of_the_week:
+            return date(year,month,5)
+        if date(year,month,6).weekday() == day_of_the_week:
+            return date(year,month,6)
+        if date(year,month,7).weekday() == day_of_the_week:
+            return date(year,month,7)
+    if which == '2nd':
+        if date(year,month,8).weekday() == day_of_the_week:
+            return date(year,month,8)
+        if date(year,month,9).weekday() == day_of_the_week:
+            return date(year,month,9)
+        if date(year,month,10).weekday() == day_of_the_week:
+            return date(year,month,10)
+        if date(year,month,11).weekday() == day_of_the_week:
+            return date(year,month,11)
+        if date(year,month,12).weekday() == day_of_the_week:
+            return date(year,month,12)
+        if date(year,month,13).weekday() == day_of_the_week:
+            return date(year,month,13)
+        if date(year,month,14).weekday() == day_of_the_week:
+            return date(year,month,14)
+    if which == '3rd':
+        if date(year,month,15).weekday() == day_of_the_week:
+            return date(year,month,15)
+        if date(year,month,16).weekday() == day_of_the_week:
+            return date(year,month,16)
+        if date(year,month,17).weekday() == day_of_the_week:
+            return date(year,month,17)
+        if date(year,month,18).weekday() == day_of_the_week:
+            return date(year,month,18)
+        if date(year,month,19).weekday() == day_of_the_week:
+            return date(year,month,19)
+        if date(year,month,20).weekday() == day_of_the_week:
+            return date(year,month,20)
+        if date(year,month,21).weekday() == day_of_the_week:
+            return date(year,month,21)
+    if which == '5th' or which == 'last':
+        if date(year,month,29).weekday() == day_of_the_week:
+            return date(year,month,29)
+        if date(year,month,30).weekday() == day_of_the_week:
+            return date(year,month,30)
+        if date(year,month,31).weekday() == day_of_the_week:
+            return date(year,month,31)
+    if which == '4th' or which == 'last':
+        if date(year,month,22).weekday() == day_of_the_week:
+            return date(year,month,22)
+        if date(year,month,23).weekday() == day_of_the_week:
+            return date(year,month,23)
+        if date(year,month,24).weekday() == day_of_the_week:
+            return date(year,month,24)
+        if date(year,month,25).weekday() == day_of_the_week:
+            return date(year,month,25)
+        if date(year,month,26).weekday() == day_of_the_week:
+            return date(year,month,26)
+        if date(year,month,27).weekday() == day_of_the_week:
+            return date(year,month,27)
+        if date(year,month,28).weekday() == day_of_the_week:
+            return date(year,month,28)
